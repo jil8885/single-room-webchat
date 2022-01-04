bdfda5b25ddef8d4c8314d1c683429e62bcc8d60
jeongin Lee <jil8885@hanyang.ac.kr>
2022-01-04 17:04:17 +0900

    Python-socketIO를 이용한 방 1개 서버 통신

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
-from aiohttp.web import Application, run_app
-from aiohttp.web import get
-from controller.socket import chatting_socket_controller
+import aiohttp_cors
+from aiohttp.web import Application, run_app, Response
+
+import socketio
+
+socketio_server = socketio.AsyncServer(cors_allowed_origins='*')
+web_application = Application()
+socketio_server.attach(web_application)
 
 
-async def init_webapp() -> Application:
-    # Create Web application object
-    application = Application()
-
-    # Declare dict to include socket object
-    application["websockets"] = {}
-
-    # Close all web sockets when turn off server
-    application.on_shutdown.append(close_webapp)
-
-    # Endpoint that client can request
-    application.add_routes([get("/talk", handler=chatting_socket_controller)])
-    return application
+async def open_web(request):
+    with open("index.html", encoding="utf-8") as f:
+        return Response(text=f.read(), content_type="text/html")
 
 
-async def close_webapp(app: Application):
-    for web_socket in app.get("websocket").values():
-        web_socket.close()
-    app.get("websockets").clear()
+@socketio_server.on("connect", namespace="/talk")
+def connect_server(event_id, data):
+    print("connect")
+
+
+@socketio_server.on('send_message', namespace='/talk')
+async def message(event_id, data):
+    print("message received ", data)
+    await socketio_server.emit('received_message', {'data': data}, namespace='/talk')
+
+
+@socketio_server.on('disconnect', namespace='/talk')
+def disconnect(event_id):
+    print('disconnect ')
+
+
+web_application.router.add_get("/", open_web)
+web_application.router.add_static("/static", "css")
+cors = aiohttp_cors.setup(web_application, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+        allow_credentials=True,
+        expose_headers="*",
+        allow_headers="*",
+    )
+})
+
 
 
 def main():
-    application = init_webapp()
-    run_app(application)
+    run_app(web_application)
 
 
 if __name__ == '__main__':
